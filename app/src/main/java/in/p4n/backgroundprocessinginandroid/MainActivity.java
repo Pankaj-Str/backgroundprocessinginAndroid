154c0e2c80c5122e711964f98f6b3fe9595b0704
pankajchouhan <pankajchouhan80@gmail.com>
2019-01-22 11:00:43 +0530

    Initial commit

---
+++ app/src/main/java/in/p4n/backgroundprocessinginandroid/MainActivity.java
@@ -0,0 +1,49 @@
+package in.p4n.backgroundprocessinginandroid;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progress;
+    private TextView text;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progress = (ProgressBar) findViewById(R.id.progressBar1);
+        text = (TextView) findViewById(R.id.textView1);
+
+    }
+
+    public void startProgress(View view) {
+        // do something long
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 10; i++) {
+                    final int value = i;
+                    doFakeWork();
+                    progress.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText("Updating");
+                            progress.setProgress(value);
+                        }
+                    });
+                }
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+    // Simulating something timeconsuming
+    private void doFakeWork() {
+        SystemClock.sleep(5000);
+    }
+
+}
